3dfd7ae84e0391111fd1c668e10675f910cbd4cb
Wamique Abro <wamik.abro212@gmail.com>
2023-12-30 14:18:26 +0500

    create separate control for RadioBtutton to control Navigation RadioButtons.

---
+++ Quora Desktop/Utilities/Btn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+namespace Quora_Desktop.Utilities
+{
+    public class Btn : RadioButton
+    {
+        static Btn()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Btn), new FrameworkPropertyMetadata(typeof(Btn)));
+        }
+    }
+}
