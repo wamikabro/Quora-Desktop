d65bec9c7d8252356ea422013cfc14a5e280d25b
Wamique Abro <wamik.abro212@gmail.com>
2024-01-04 17:49:08 +0500

    add page model

--- Quora Desktop/ViewModel/HomeVM.cs
+++ Quora Desktop/ViewModel/HomeVM.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Quora_Desktop.ViewModel
 {
-    internal class HomeVM
+    class HomeVM
     {
     }
 }
