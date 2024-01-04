dfac61a1f907811d88205bb7d2d705026f22d28a
Wamique Abro <wamik.abro212@gmail.com>
2024-01-04 17:57:19 +0500

    Connect HomeVM to PageModel

--- Quora Desktop/ViewModel/HomeVM.cs
+++ Quora Desktop/ViewModel/HomeVM.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quora_Desktop.Model;
 namespace Quora_Desktop.ViewModel
 {
-    class HomeVM
+    class HomeVM : Utilities.ViewModelBase
     {
+        private readonly PageModel _pageModel;
+        public int UserID
+        {
+            get { return _pageModel.UserID; }
+            set { _pageModel.UserID = value; OnPropertyChanged(); }
+        }
+
+        public HomeVM()
+        {
+            _pageModel= new PageModel();
+            UserID = 0;
+        }
     }
 }
